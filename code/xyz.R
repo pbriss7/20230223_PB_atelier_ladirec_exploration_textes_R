e9f7b931861cc1fef9a628e3dcb8ca7e73d5606d
Pascal Brissette <pascal.brissette@mcgill.ca>
2023-02-22 21:00:49 -0500

    Mise à jour xyz.R

--- code/xyz.R
+++ code/xyz.R
@@ -1,63 +1,34 @@
-#### Exploration des données textuelles avec R - 23 février 2023 ----
-
-## Note importante
-# Une ligne précédée d'un ou de plusieurs croisillons # ne sera pas exécutée par l'interpréteur
-# Pour exécuter une instruction, placez votre curseur au début de la ligne et appuyez sur 'COMMAND' + 'RETURN' (raccourci Mac) ou 'CTRL' + 'RETURN' (raccourci Windows)
-# Pour obtenir de l'aide sur une fonction, exécutez-la précédée d'un point d'interrogation. Par exemple:
-
-
-
-## ----setup, include=FALSE---------------------------------------------------------------------------------------
 knitr::opts_chunk$set(warning = FALSE, message = FALSE)
 
 
 
-## ---------------------------------------------------------------------------------------------------------------
-
+# Création d'un vecteur comprenant trois titres de romans
 romans <- c("Illusions perdues", "Le Dernier jour d'un condamné", "La Débâcle")
 
-cat("Mon roman préféré est ", sample(romans, 1), ".", sep = "")
+# Impression d'une chaine de caractères et d'une fonction qui choisit au hasard l'un des trois éléments du vecteur romans
+cat("Mon roman préféré est ", sample(x = romans, size = 1), ".", sep = "")
 
-
-
-## ---------------------------------------------------------------------------------------------------------------
-
-romans <- c("Illusions perdues", "Le Dernier jour d'un condamné", "La Débâcle")
-
-cat("Mon roman préféré est ", sample(romans, 1), ".", sep = "")
-
-
-
-## ---------------------------------------------------------------------------------------------------------------
 
 ?paste()
 ?str()
-?install.packages()
-?library()
 ?nchar()
-?data.frame()
 
 
 
-## ---------------------------------------------------------------------------------------------------------------
 # Création d'objets à éliminer
 a=1
 b=3
 c="Virginie"
 d="Théodore"
 
-# Élimination de tout le contenu de l'environnement:
+# Élimination d'une sélection d'éléments
+rm(list = setdiff(ls(), c("d", "c")))
 
+# Élimination de tout le contenu de l'environnement:
 rm(list = ls()) 
 
 
-# Élimination d'une sélection d'éléments
-rm(list = setdiff(ls(), c("d")))
 
-
-
-
-## ---------------------------------------------------------------------------------------------------------------
 
 # Installation des extensions dont nous aurons besoin (si elles ne sont pas déjà installées)
 if(!"stringr" %in% rownames(installed.packages())) {install.packages("stringr")}
@@ -68,6 +39,7 @@ if(!"quanteda" %in% rownames(installed.packages())) {install.packages("quanteda"
 if(!"quanteda.textplots" %in% rownames(installed.packages())) {install.packages("quanteda.textplots")}
 if(!"quanteda.textstats" %in% rownames(installed.packages())) {install.packages("quanteda.textstats")}
 if(!"lsa" %in% rownames(installed.packages())) {install.packages("lsa")}
+if(!"janitor" %in% rownames(installed.packages())) {install.packages("janitor")}
 
 # Activation des extensions
 library(readxl)               # Extension pour l'importation de fichiers Excel
@@ -78,18 +50,17 @@ library(quanteda)             # Extension pour le forage textuel
 library(quanteda.textplots)   # Extension pour le forage textuel
 library(quanteda.textstats)   # Extension pour l'analyse des collocations
 library(lsa)                  # Extension offrant un antidictionnaire élaboré
+library(janitor)              # Extension qui offre des fonctions pour le "nettoyage" de chaines de caractères
 
 
 
-## ---------------------------------------------------------------------------------------------------------------
+# Lecture des données et assignation à une variable
+# Les deux points suivis de / indiquent à R qu'il doit remonter d'un niveau pour trouver un dossier appelé `donnees`
 
-# Lecture des données et assignation à une variable 
 xyz <- readxl::read_excel("../donnees/XYZ-2015-2022-table-20230205JV.xlsx", sheet = 1)
 
 
 
-
-## ---------------------------------------------------------------------------------------------------------------
 # Structure de l'objet
 str(xyz)
 
@@ -114,8 +85,6 @@ class(xyz)
 # Type de données d'un vecteur du tableau
 class(xyz$`ISSN (numérique)`)
 
-
-## ---------------------------------------------------------------------------------------------------------------
 # La première fonction renvoie, pour chaque colonne du tableau, la somme des valeurs correspondant à NA.
 sapply(xyz, function(x) sum(is.na(x)))
 
@@ -142,9 +111,7 @@ colnames(xyz)
 
 
 
-## ---------------------------------------------------------------------------------------------------------------
-
-
+# On assigne aux noms de colonnes un vecteur comprenant, pour chaque colonne, le nom choisi, dans le bon ordre.
 colnames(xyz) <- c("periodique", "titre", "auteur",
                    "numero", "date", "theme", "uri",
                    "editeur", "issn_imp", "issn_num",
@@ -153,15 +120,19 @@ colnames(xyz) <- c("periodique", "titre", "auteur",
 
 
 
-## ---------------------------------------------------------------------------------------------------------------
+# Nous allons créer une petite fonction maison qui va générer des identifiants uniques composé d'une chaine de caractères commençant par le nom de l'auteur du texte, suivi du numéro de la ligne du document dans le tableau. Ces éléments seront concaténés. La fonction recourra ensuite à la fonction `make_clean_names()` de l'extension janitor pour assurer un résultat propre et uniforme.
+
+faire_unique_id_fonction <- function(vecteur1, vecteur2){
+  valeurs_concatenees <- paste(vecteur1, vecteur2, sep = "_")
+  valeurs_concatenees <- janitor::make_clean_names(valeurs_concatenees)
+  return(valeurs_concatenees)
+}
+
+# Nous passons à notre fonction les deux vecteurs qui forment le matériau de nos doc_id, et emmagasinons le résultat dans une nouvelle colonne du tableau appelée `doc_id`.
+xyz$doc_id <- faire_unique_id_fonction(vecteur1 = xyz$auteur, vecteur2 = 1:nrow(xyz))
 
 
-xyz$doc_id <- 1:nrow(xyz)
 
-
-
-
-## ---------------------------------------------------------------------------------------------------------------
 
 # La première colonne semble contenir une information redondante. Vérifions:
 unique(xyz[ , "periodique"])  # ou table(xyz$periodique)
@@ -173,11 +144,9 @@ unique(xyz[, "theme"])        # ou table(xyz$theme)
 # unique(...[, ...])
 
 
-
-## ---------------------------------------------------------------------------------------------------------------
 # Solution au problème no 3
 
-# Créons un vecteur avec les colonnes inutiles (on utilise pour cela la fonction de concaténation c( ) )
+# On crée un vecteur avec les colonnes inutiles
 colonnes_a_supprimer <- c("periodique", "editeur", "issn_imp", "issn_num", "mention_legale", "uri", "citation")
 
 
@@ -185,8 +154,6 @@ colonnes_a_supprimer <- c("periodique", "editeur", "issn_imp", "issn_num", "ment
 xyz[, colonnes_a_supprimer] <- NULL
 
 
-
-## ---------------------------------------------------------------------------------------------------------------
 
 # Il faut extraire les années de la colonne `date`. On utilise pour cela l'extension stringr.
 
@@ -197,8 +164,6 @@ xyz$annee <- stringr::str_extract(xyz$date, "[0-9]+")
 # xyz$... <- ...
 
 
-
-## ---------------------------------------------------------------------------------------------------------------
 
 # La fonction as.integer() force la conversion du type chararcter en type integer.
 
@@ -211,14 +176,15 @@ xyz$numero <- as.integer(xyz$numero)
 
 
 
-## ---------------------------------------------------------------------------------------------------------------
-
 # Observons tout d'abord un texte en particulier
 # xyz$texte[1]
 
 
 # Remplaçons par une espace simple le symbole `\n`
-xyz$texte <- gsub(pattern = "\n", replacement = " ", x = xyz$texte, fixed = TRUE)
+xyz$texte <- gsub(pattern = "\n",
+                  replacement = " ",
+                  x = xyz$texte,
+                  fixed = TRUE) # On indique ici à R de ne pas lire le "pattern" comme une                                       expression régulière, mais littéralement
 
 
 ### Exercice: vérifiez un texte pris au hasard pour voir s'il reste des scories
@@ -226,16 +192,15 @@ xyz$texte <- gsub(pattern = "\n", replacement = " ", x = xyz$texte, fixed = TRUE
 
 
 
-## ---------------------------------------------------------------------------------------------------------------
-
 xyz <- xyz[, c("doc_id", "auteur", "titre", "numero", "annee", "theme", "texte")]
 
 
 
-## ---------------------------------------------------------------------------------------------------------------
-# La fonction `table( )` permet d'observer le nombre de modalités d'une variable. 
+# La fonction `table( )` permet d'observer le nombre de modalités uniques d'un vecteur 
 distrib_annuelle <- table(xyz$annee)
-# distrib_annuelle
+
+# Observons le résultat en appelant les 10 premiers éléments de L'objet `table`
+distrib_annuelle[1:10]
 
 # Quelle est la moyenne de cette distribution?
 mean(distrib_annuelle)
@@ -245,7 +210,7 @@ mean(distrib_annuelle)
 
 
 
-## ---------------------------------------------------------------------------------------------------------------
+sort(distrib_annuelle, decreasing = TRUE)
 
 # distrib_themes_ord <- sort(distrib_themes, decreasing = TRUE)
 
@@ -256,14 +221,13 @@ mean(distrib_annuelle)
 
 
 
-## ---------------------------------------------------------------------------------------------------------------
-# 1. Syntaxe étendue (petite perte de mémoire, mais plus explicite: le résultat de chaque opération est emmagasiné dans une variable)
+# 1. Syntaxe étendue -- plus explicite: le résultat de chaque opération est emmagasiné dans des conteneurs indépendants, sollicitant davantage la mémoire
 distrib_auteurs <- table(xyz$auteur)
 distrib_auteurs_ord <- sort(distrib_auteurs, decreasing = TRUE)
 head(distrib_auteurs_ord, n = 10)
 
 
-# 2. Syntaxe condensée, par enchâssement des fonctions. Lire de l'intérieur vers l'extérieur
+# 2. Syntaxe condensée: enchâssement des fonctions -- lire de l'intérieur vers l'extérieur
 head(sort(table(xyz$auteur), decreasing = TRUE), 10)
 
 
@@ -273,24 +237,23 @@ xyz$auteur |> table() |> sort(decreasing = TRUE) |> head(10)
 
 
 
-## ---------------------------------------------------------------------------------------------------------------
-
-mtcars[1:3] # Dans le jeu de données, les noms de lignes (rownames) correspondent aux marques.
+mtcars[1:3, 1:3] # Dans le jeu de données, les noms de lignes (rownames) correspondent aux marques
 
 # Dans ce graphique simple, on pose la variable dépendante en y et l'indépendante en x
 plot(mtcars$disp, mtcars$mpg)
 
 
 
-## ---------------------------------------------------------------------------------------------------------------
-
 cor.test(mtcars$disp, mtcars$mpg, method = "kendall")
 
 
 
-## ---------------------------------------------------------------------------------------------------------------
+# Dans R, la plupart des fonctions de base sont vectorisées, c'est-à-dire que si on lui donne en entrée un vecteur, la fonction effectuera l'opération demandée sur chacun de ses éléments. D'où la grande simplicité des instructions suivantes:
 
+# Nombre total de caractères pour chaque titre
 xyz$ncharTitre <- nchar(xyz$titre)
+
+# Nombre total de caractères pour chaque texte
 xyz$ncharTexte <- nchar(xyz$texte)
 
 
@@ -300,57 +263,46 @@ xyz[, c("ncharTitre", "ncharTexte")]
 
 
 
-## ---------------------------------------------------------------------------------------------------------------
-
 cor.test(xyz$ncharTitre, xyz$ncharTexte, method = "kendall")
 
 
 
-
-## ---------------------------------------------------------------------------------------------------------------
 
 ggplot(xyz, aes(x=ncharTitre, y=ncharTexte))+
   geom_jitter()+
   geom_smooth()
 
 
-## ---------------------------------------------------------------------------------------------------------------
 
 # On peut tout d'abord observer le sommaire statistique et s'en inspirer
-summary(xyz$ncharTexte)    
+summary(xyz$ncharTexte)
 
-xyz$longueur_texte_cat <- factor(                                # Création de valeurs catégorielles fondées sur les modalités d'une autre colonne
+# Création de valeurs catégorielles fondées sur les modalités d'une autre colonne
+# La fonction `factor()` possède un argument, `levels=` qui permet de déterminer l'ordre des catégories
+xyz$longueur_texte_cat <- factor(                                
   ifelse(xyz$ncharTexte < 5000, "court",
          ifelse(xyz$ncharTexte >10000, "long", "moyen")),
-  levels = c("court", "moyen", "long")                           # La fonction `factor()` possède un argument, `levels=` qui permet de déterminer l'ordre des catégories
+  levels = c("court", "moyen", "long")                        
 )
 
 # Exercice: observez la distribution de cette variable avec la fonction table().
 
 
 
-## ---------------------------------------------------------------------------------------------------------------
-
 ggplot(xyz, aes(x = longueur_texte_cat))+                        
   geom_bar(stat = "count")
 
 
 
-## ---------------------------------------------------------------------------------------------------------------
-
 ?quanteda::corpus()
 
 
-
-## ---------------------------------------------------------------------------------------------------------------
 
 xyz_corp <- quanteda::corpus(xyz, docid_field = "doc_id", text_field = "texte")   
 
 head(xyz_corp, 2)
 
 
-
-## ---------------------------------------------------------------------------------------------------------------
 xyz_toks <- tokens(xyz_corp, 
                    remove_punct = TRUE,              # On supprime à la volée la ponctuation
                    remove_symbols = TRUE,            # On supprime à la volée les symboles
@@ -361,8 +313,6 @@ xyz_toks <- tokens(xyz_corp,
 head(xyz_toks, 2)
 
 
-
-## ---------------------------------------------------------------------------------------------------------------
 
 # On transforme l'objet tokens en dfm
 xyz_dfm <- dfm(xyz_toks) |>
@@ -378,13 +328,11 @@ xyz_dfm <- dfm(xyz_toks) |>
 head(xyz_dfm, 2)
 
 ### Exercice: modifiez les seuils et voyez l'effet sur les dimensions de l'objet!
-# Pour voir les dimensions de l'objet, vous n'avez qu'à l'apeller ainsi:
+# Pour voir les dimensions de l'objet, vous n'avez qu'à l'appeler ainsi:
+
+xyz_dfm
 
 
-
-
-
-## ---------------------------------------------------------------------------------------------------------------
 
           
 xyz_dfm_rioux <- dfm_subset(xyz_dfm, subset = auteur == "Hélène Rioux")
@@ -398,8 +346,6 @@ textplot_wordcloud(xyz_dfm_dorais, max_words = 200)
 
 
 
-
-## ---------------------------------------------------------------------------------------------------------------
 
 dfm_comp_auteurs <- xyz_corp |> 
   
@@ -428,8 +374,6 @@ textplot_wordcloud(dfm_comp_auteurs_groupes, comparison = TRUE, max_words = 300,
 
 
 
-## ---------------------------------------------------------------------------------------------------------------
-
 xyz_dfm_th_jardin <- dfm_subset(xyz_dfm, subset = theme == "Jardin : un enfer de morceaux de paradis")
 
 xyz_dfm_th_YOLO <- dfm_subset(xyz_dfm, subset = theme == "YOLO (You Only Live Once) : hardis, téméraires, écervelés, aventureux, fonceurs, délurés")
@@ -445,8 +389,6 @@ textplot_wordcloud(xyz_dfm_th_YOLO)
 
 #### Exercice: choisissez deux autres numéros thématiques et comparez les vocabulaires. Vous pouvez utiliser la voie simple ou reprendre le bloc d'instructions enchainées ci-dessus.
 
-
-## ---------------------------------------------------------------------------------------------------------------
 
   # On fournit en entrée l'objet tokens créé auparavant
 xyz_fcm <- xyz_toks |>
@@ -471,8 +413,6 @@ textplot_network(fcm_select(xyz_fcm, principaux_mots), min_freq = 0.7)
 
 
 
-## ---------------------------------------------------------------------------------------------------------------
-
 # Conversion de l'objet fcm en simple matrice
 xyz_cooc_matrice <- convert(xyz_fcm, "matrix")
 
@@ -481,9 +421,6 @@ str(xyz_cooc_matrice)
 
 # On peut donc indexer la matrice avec les mots à observer. La valeur renvoyée correspondra au nombre de fois que ces mots cooccurrent dans la fenêtre de 10 mots.
 xyz_cooc_matrice["jeune", "femme"]
-
-
-## ---------------------------------------------------------------------------------------------------------------
 
 xyz_corp |> 
 
@@ -497,8 +434,7 @@ xyz_corp |>
   # tokens_remove(lsa::stopwords_fr) |>
   
   kwic(pattern = phrase("jeunes? femmes?"), 
-       window = 3, 
-       valuetype = "regex") |> head()
-
+       window = 5, 
+       valuetype = "regex") |> head(10)
 
 
